3ba8b7c13e44e2646825370a9735fc6cfe85f2d8
root <me@txus.io>
2020-01-21 02:30:56 +0000

    Update training

--- launch.py
+++ launch.py
@@ -5,6 +5,8 @@ import sys
 
 lrank = sys.argv[1].split('=')[1]
 
-s = f"python -m calbert train_model --tokenizer-dir /root/tokenizer --dataset-dir /root/dataset --out-dir /root/model --tensorboard-dir /root/tensorboard --per_gpu_train_batch_size 32 --per_gpu_eval_batch_size 32 --subset 1.0 --wandb True --local-rank {lrank}"
+batch_size = 96
+
+s = f"python -m calbert train_model --tokenizer-dir /root/tokenizer --dataset-dir /root/dataset --out-dir /root/model --tensorboard-dir /root/tensorboard --per_gpu_train_batch_size {batch_size} --per_gpu_eval_batch_size {batch_size} --subset 1.0 --wandb True --local-rank {lrank}"
 
 subprocess.run(s.split(' '))
