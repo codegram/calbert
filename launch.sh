65bbe1fb8cd58a48eb18055213cc5dce1702f659
Txus <me@txus.io>
2020-01-22 14:06:04 +0900

    Remove old launch script

--- launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-spell run -v --machine-type v100 --mount runs/315/dataset:/spell/dataset --mount runs/314/tokenizer:/spell/tokenizer --from codegram/calbert:latest 'python -m calbert train_model --tokenizer-dir /spell/tokenizer --dataset-dir /spell/dataset --out-dir /spell/model --tensorboard-dir /spell/tensorboard --per_gpu_train_batch_size 128 --per_gpu_eval_batch_size 128 --subset 0.1 --wandb True'
